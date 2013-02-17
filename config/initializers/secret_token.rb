45590d110a0e927ffd6482cceec2f53fc728982f
MacBook <macbook@MacBooks-MacBook.local>
2013-02-16 21:26:35 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '4020a4029610dc1080849f68c5886c9d102b87e72fecca2c474aec485a0a420c0f5ea247247d553095e9d509c2204f111124e3a333aaac0e3086997f50b96cd9'
